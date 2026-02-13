dd3f3e6ac6f6faf27f024160719ec1dec4a60618
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2026-02-13 19:05:28 +0400

    feat product tables

---
+++ app/(products)/products-list/components/ProductsTable.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { Box, Paper } from "@mui/material";
+import {
+  DataGrid,
+  GridColDef,
+  GridPaginationModel,
+  GridRowsProp,
+} from "@mui/x-data-grid";
+import { useState } from "react";
+import { refetchProducts } from "../services/index.client";
+
+interface ProductsTableProps {
+  columns: GridColDef[];
+  rows: GridRowsProp;
+  totalRows: number;
+}
+
+export default function ProductsTable({
+  columns,
+  rows,
+  totalRows,
+}: ProductsTableProps) {
+  const [page, setPage] = useState(1);
+  const [products, setProducts] = useState(rows);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleChangePage = async (newPage: number, newPerPage: number) => {
+    const res = await refetchProducts(newPage + 1, newPerPage);
+
+    if (res.status === 200) {
+      setProducts(res.data.products);
+    }
+  };
+
+  const handlePaginationModelChange = (model: GridPaginationModel) => {
+    const newPage = model.page;
+    const newPageSize = model.pageSize;
+
+    setPage(newPage);
+    setRowsPerPage(newPageSize);
+    handleChangePage(newPage, newPageSize);
+  };
+
+  return (
+    <Box sx={{ width: "100%", height: "100%" }}>
+      <Paper
+        elevation={3}
+        sx={{
+          height: "100%",
+          width: "100%",
+          borderRadius: 3,
+          overflow: "hidden",
+        }}
+      >
+        <DataGrid
+          rows={products}
+          columns={columns}
+          rowCount={totalRows}
+          initialState={{
+            pagination: {
+              paginationModel: { pageSize: rowsPerPage, page: page - 1 },
+            },
+          }}
+          pageSizeOptions={[5, 10, 20]}
+          onPaginationModelChange={handlePaginationModelChange}
+          checkboxSelection
+          paginationMode="server"
+          disableRowSelectionOnClick
+          sx={{
+            border: 0,
+            "& .MuiDataGrid-columnHeaders": {
+              backgroundColor: "#f5f5f5",
+              fontWeight: "bold",
+            },
+          }}
+        />
+      </Paper>
+    </Box>
+  );
+}
---
+++ app/(products)/products-list/page.tsx
@@ -0,0 +1,69 @@
+import { GridColDef, GridRowsProp } from "@mui/x-data-grid";
+import ProductsTable from "./components/ProductsTable";
+import { getProductsService } from "./services";
+import { Product } from "@/types/products";
+
+export default async function ProductsList() {
+  const page = 1;
+  const limit = 10;
+  const getAllProducts = await getProductsService(page, limit);
+  const allProduct = getAllProducts.data.products as Product[];
+  const totalRows = getAllProducts.data.totalRows as number;
+
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "product_name",
+      headerName: "Product Name",
+      flex: 1,
+      minWidth: 150,
+    },
+    {
+      field: "product_description",
+      headerName: "Description",
+      flex: 1,
+      minWidth: 200,
+    },
+    {
+      field: "product_price",
+      headerName: "Price ($)",
+      type: "number",
+      width: 120,
+    },
+    {
+      field: "product_quantity",
+      headerName: "Stock",
+      type: "number",
+      width: 120,
+    },
+    {
+      field: "product_discount",
+      headerName: "Discount",
+      type: "number",
+      width: 120,
+    },
+    {
+      field: "product_weight",
+      headerName: "Weight",
+      type: "number",
+      width: 120,
+    },
+  ];
+
+  const rows: GridRowsProp = allProduct.map((product: Product) => ({
+    id: product.id,
+    product_name: product.product_name,
+    product_description: product.product_description,
+    product_price: product.product_price,
+    product_quantity: product.product_quantity,
+    product_discount: product.product_discount,
+    product_weight: product.product_weight,
+  }));
+
+  return (
+    <div className="p-4 space-y-4">
+      <h1 className="text-3xl font font-bold">Products List</h1>
+      <ProductsTable totalRows={totalRows} columns={columns} rows={rows} />
+    </div>
+  );
+}
